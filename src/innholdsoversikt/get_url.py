45ed651c789eab26e375ac082b08d23cda264d4f
tobiasmcvey <tobias.mcvey@gmail.com>
2023-08-21 11:57:44 +0200

    fjern ubrukte avhengigheter (#11)
    * fjern ubrukte avhengighetene pyarrow og xmltodict for å redusere størrelse på docker image
    
    * fjern ubrukte metoder knyttet til xmltodict
    
    * DRY forenkler Dockerfile ved å bruke miljøvariabler istedet for å gjenta aktivering av virtualenv

--- src/innholdsoversikt/get_url.py
+++ src/innholdsoversikt/get_url.py
@@ -3,7 +3,6 @@ import logging
 
 import pandas as pd
 import requests
-import xmltodict
 from urllib.parse import urlparse
 
 logging.getLogger(__name__)
@@ -12,48 +11,6 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
-
-
-# %%
-def get_url_from_sitemap(url: str) -> pd.DataFrame:
-    """
-    Parses an XML sitemap and returns all URLs as a DataFrame
-
-    Parameters
-    ---------
-    url: str, required
-        URL to the sitemap
-
-    Returns
-    ----------
-    df = DataFrame
-    """
-    res = requests.get(url)
-    raw = xmltodict.parse(res.text)
-
-    data = [[r["loc"]] for r in raw["urlset"]["url"]]
-    df = pd.DataFrame(data, columns=["links"])
-    logging.info("Returning all URLs for the URL %s", url)
-    return df
-
-
-# %%
-def get_sitemaps_from_domain(domain: str) -> pd.DataFrame:
-    """
-    Gets all sitemaps from a sitemap index file if it exists for a given domain.
-
-    Parameters:
-    ---------
-    domain: str, required
-        URL to the sitemap index file
-    """
-    res = requests.get(domain)
-    raw = xmltodict.parse(res.text)
-
-    data = [[r["loc"]] for r in raw["sitemapindex"]["sitemap"]]
-    df = pd.DataFrame(data, columns=["links"])
-    logging.info("Returning all sitemaps for the domain %s", domain)
-    return df
 
 
 # %%
